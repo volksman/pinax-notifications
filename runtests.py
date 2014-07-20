6782099acf104803185172b046a2ae1ad4e4c3cd
Patrick Altman <paltman@gmail.com>
2014-07-20 11:13:56 -0400

    Update travis settings

--- runtests.py
+++ runtests.py
@@ -1,16 +1,13 @@
+#!/usr/bin/env python
+import os
 import sys
+
+import django
 
 from django.conf import settings
 
-settings.configure(
-    DEBUG=True,
-    USE_TZ=True,
-    DATABASES={
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-        }
-    },
-    ROOT_URLCONF="notification.urls",
+
+DEFAULT_SETTINGS = dict(
     INSTALLED_APPS=[
         "django.contrib.auth",
         "django.contrib.contenttypes",
@@ -19,15 +16,42 @@ settings.configure(
         "notification",
         "notification.tests",
     ],
-    STRIPE_PUBLIC_KEY="",
-    STRIPE_SECRET_KEY="",
-    PAYMENTS_PLANS={},
+    DATABASES={
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": ":memory:",
+        }
+    },
+    SITE_ID=1,
+    ROOT_URLCONF="notification.urls",
+    SECRET_KEY="notasecret",
 )
 
-from django_nose import NoseTestSuiteRunner
 
-test_runner = NoseTestSuiteRunner(verbosity=1)
-failures = test_runner.run_tests(["notification"])
+def runtests(*test_args):
+    if not settings.configured:
+        settings.configure(**DEFAULT_SETTINGS)
 
-if failures:
+    # Compatibility with Django 1.7's stricter initialization
+    if hasattr(django, "setup"):
+        django.setup()
+
+    parent = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, parent)
+
+    try:
+        from django.test.runner import DiscoverRunner
+        runner_class = DiscoverRunner
+        test_args = ["notification.tests"]
+    except ImportError:
+        from django.test.simple import DjangoTestSuiteRunner
+        runner_class = DjangoTestSuiteRunner
+        test_args = ["tests"]
+
+    failures = runner_class(
+        verbosity=1, interactive=True, failfast=False).run_tests(test_args)
     sys.exit(failures)
+
+
+if __name__ == "__main__":
+    runtests(*sys.argv[1:])
