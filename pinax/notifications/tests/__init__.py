93516986e87bd84d3a07aa67f93af244dd48355e
Patrick Altman <paltman@gmail.com>
2015-03-03 08:32:31 -0600

    Delay the import

--- pinax/notifications/tests/__init__.py
+++ pinax/notifications/tests/__init__.py
@@ -1,7 +1,5 @@
-from ..models import NOTICE_MEDIA
-
-
 def get_backend_id(backend_name):
+    from ..models import NOTICE_MEDIA
     for bid, bname in NOTICE_MEDIA:
         if bname == backend_name:
             return bid
