a2c20e41426abcb808a898dcb788990c8bb836e7
Jacob Wegner <jacobwegner@gmail.com>
2016-11-04 12:38:31 -0500

    bump version to 4.0.1 for future releases

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description="User notification management for the Django web framework",
     name="pinax-notifications",
     long_description=read("README.rst"),
-    version="4.0.0",
+    version="4.0.1",
     url="http://pinax-notifications.rtfd.org/",
     license="MIT",
     packages=find_packages(),
